e671cca54d3e725ed033b513e8e602f1393a44a0
Dmitry Kovalskiy <dmitry.kovalskiy@me.com>
2016-05-24 17:29:36 +0300

    checkin

---
+++ Testing/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using TicketToRide;
+namespace Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<TicketCard> source = new List<TicketCard>();
+
+            source.Add(new TicketCard() { Departure = "Москва", Arrival = "Париж" });
+            source.Add(new TicketCard() { Departure = "Кельн", Arrival = "Москва" });
+            source.Add(new TicketCard() { Departure = "Мельбурн ", Arrival = "Кельн" });
+
+            List<TicketCard> result = new List<TicketCard>();
+
+            result.Add(new TicketCard() { Departure = "Мельбурн ", Arrival = "Кельн" });
+            result.Add(new TicketCard() { Departure = "Кельн", Arrival = "Москва" });
+            result.Add(new TicketCard() { Departure = "Москва", Arrival = "Париж" });
+
+            var methodResult = CartOrder.Order(source);
+
+            Assert.AreEqual(result.Count, methodResult.Count);
+            for (int i = 0; i < result.Count; i++)
+            {
+                Assert.AreEqual(result[i].Arrival, methodResult[i].Arrival);
+                Assert.AreEqual(result[i].Departure, methodResult[i].Departure);
+            }
+        }
+    }
+}
---
+++ TicketToRide/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketToRide
+{
+    public class CartOrder
+    {
+        static void  Main()
+        { }
+        public static List<TicketCard> Order(List<TicketCard> source)
+        {
+            List<TicketCard> result = new List<TicketCard>();
+
+            foreach (var item in source)
+            {
+                result = new List<TicketCard>();
+                var cloneSource = source.Where(c => true).ToList();
+                result.Add(item);
+                var root = item;
+                while (HasNext(cloneSource, root))
+                {
+                    root = GetNext(cloneSource, root);
+                    result.Add(root);
+                }
+                if (source.Count == result.Count)
+                    return result;
+            }
+            return result;
+        }
+
+        private static TicketCard GetNext(List<TicketCard> source, TicketCard item)
+        {
+            var result = source.Where(c => c.Departure == item.Arrival).First();
+            source.Remove(result);
+            return result;
+        }
+
+        private static bool HasNext(List<TicketCard> source, TicketCard item)
+        {
+            return source.Any(c => c.Departure == item.Arrival);
+        }
+    }
+
+   public class TicketCard
+    {
+        public string Departure { get; set; }
+        public string Arrival { get; set; }
+    }
+}
